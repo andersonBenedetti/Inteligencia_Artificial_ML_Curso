a611c09bed6a2287739468becc0f137558f63a0d
Anderson Benedetti <andersonjcbenedetti@gmail.com>
2024-09-27 01:09:50 -0300

    first commit

---
+++ Algoritmos_de_Busca/valor_ordenado.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class VetorOrdenado:
+  
+  def __init__(self, capacidade):
+    self.capacidade = capacidade
+    self.ultima_posicao = -1
+    self.valores = np.empty(self.capacidade, dtype=int)
+
+  
