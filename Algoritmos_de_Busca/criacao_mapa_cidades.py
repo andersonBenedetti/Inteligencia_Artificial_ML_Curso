ae976ecd314d2f2ba5e58f6f9a914ea9af339eaf
Anderson Benedetti <andersonjcbenedetti@gmail.com>
2024-10-09 00:11:48 -0300

    feat: add busca gulosa

--- Algoritmos_de_Busca/criacao_mapa_cidades.py
+++ Algoritmos_de_Busca/criacao_mapa_cidades.py
@@ -86,5 +86,76 @@ class Grafo:
 # Instanciando o grafo
 grafo = Grafo()
 
-# Testando o método de mostrar adjacentes
-grafo.arad.mostra_adjacentes()
+import numpy as np
+class VetorOrdenado:
+
+  def __init__(self, capacidade):
+    self.capacidade = capacidade
+    self.ultima_posicao = -1
+    # Mudança no tipo de dados
+    self.valores = np.empty(self.capacidade, dtype=object)
+
+  # Referência para o vértice e comparação com a distância para o objetivo
+  def insere(self, vertice):
+    if self.ultima_posicao == self.capacidade - 1:
+      print('Capacidade máxima atingida')
+      return
+    posicao = 0
+    for i in range(self.ultima_posicao + 1):
+      posicao = i
+      if self.valores[i].distancia_objetivo > vertice.distancia_objetivo:
+        break
+      if i == self.ultima_posicao:
+        posicao = i + 1
+    x = self.ultima_posicao
+    while x >= posicao:
+      self.valores[x + 1] = self.valores[x]
+      x -= 1
+    self.valores[posicao] = vertice
+    self.ultima_posicao += 1
+
+  def imprime(self):
+    if self.ultima_posicao == -1:
+      print('O vetor está vazio')
+    else:
+      for i in range(self.ultima_posicao + 1):
+        print(i, ' - ', self.valores[i].rotulo, ' - ', self.valores[i].distancia_objetivo)
+
+vetor = VetorOrdenado(5)
+vetor.insere(grafo.arad)
+vetor.insere(grafo.craiova)
+vetor.insere(grafo.bucharest)
+vetor.insere(grafo.dobreta)
+
+vetor.imprime()
+
+vetor.insere(grafo.lugoj)
+vetor.imprime()
+
+print(vetor.valores[0], vetor.valores[0].rotulo)
+
+class Gulosa:
+  def __init__(self, objetivo):
+    self.objetivo = objetivo
+    self.encontrado = False
+
+  def buscar(self, atual):
+    print('-------')
+    print('Atual: {}'.format(atual.rotulo))
+    atual.visitado = True
+
+    if atual == self.objetivo:
+      self.encontrado = True
+    else:
+      vetor_ordenado = VetorOrdenado(len(atual.adjacentes))
+      for adjacente in atual.adjacentes:
+        if adjacente.vertice.visitado == False:
+          adjacente.vertice.visitado == True
+          vetor_ordenado.insere(adjacente.vertice)
+      vetor_ordenado.imprime()
+
+      if vetor_ordenado.valores[0] != None:
+        self.buscar(vetor_ordenado.valores[0])
+
+busca_gulosa = Gulosa(grafo.bucharest)
+busca_gulosa.buscar(grafo.arad)
